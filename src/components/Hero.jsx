bb8553550462d22c5c622d7a57156635b8a18126
riyad3399 <riyad3399>
2023-10-25 00:41:06 +0600

    added the Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,42 @@
+import riyad from "../assets/riyad.png";
+import {
+  SlSocialLinkedin,
+  SlSocialGithub,
+  SlSocialFacebook,
+} from "react-icons/sl";
 
 const Hero = () => {
-    return (
-        <div className="h-screen">
-            <h3>hero</h3>
+  const social_media = [
+    <SlSocialFacebook />,
+    <SlSocialGithub />,
+    <SlSocialLinkedin />,
+  ];
+
+  return (
+    <section className="min-h-screen py-10 flex md:flex-row flex-col  items-center">
+      <div className="flex-1 flex items-center justify-center h-full">
+        <img src={riyad} alt="photo" className="object-cover" loading="lazy"/>
+      </div>
+      <div className="flex-1 ">
+        <h4 className="md:text-5xl text-3xl md:leading-normal leading-10 text-white font-bold">
+          <span className="text-sky-600 md:text-6xl text-5xl">
+            Hello!
+            <br />
+          </span>
+          My Name is <span>Riyad khan</span>
+        </h4>
+        <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-zinc-500">
+          MERN Stack Developer
+        </h4>
+       <button className=" mt-4 btn-primary">Contact Me</button>
+        <div className="mt-8 text-2xl flex items-center md:justify-start justify-center gap-5 ">
+          {social_media.map((icon, i) => (
+            <button className="text-zinc-500 hover:text-white h-[38px] w-[38px] border rounded-full flex items-center justify-center" key={i}>{icon}</button>
+          ))}
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
+import { AiOutlineClose, AiOutlineMenuUnfold } from "react-icons/ai";
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpenMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const menuLinks = [
     { name: "HOME", link: "#home" },
@@ -21,23 +27,54 @@ const Navbar = () => {
   return (
     <nav
       className={`fixed w-full left-0 top-0 z-[999] ${
-        sticky ? "md:bg-white/60 text-gray-900" : "text-white"
+        sticky ? "md:bg-white/70 text-gray-900" : "text-white"
       }`}
     >
       <div className="flex items-center justify-between">
         <div className="mx-7">
-          <h4 className="text-4xl font-bold">
-            R<span className="text-cyan-700">iya</span>d
+          <h4 className="text-4xl font-bold uppercase ">
+            R<span className="text-sky-600 ">iya</span>d
           </h4>
         </div>
         <div
-          className={`text-gray-900 md:block hidden px-7 py-2 font-medium ${
+          className={`text-gray-900 md:block hidden px-6 py-2 font-medium ${
             sticky ? "md:bg-white/0 bg-white" : "bg-white"
-          } rounded-bl-full `}
+          } rounded-tl-full rounded-br-full `}
         >
           <ul className="flex items-center gap-1 py-2 text-lg">
             {menuLinks?.map((menu, i) => (
-              <li key={i} className="px-6 hover:text-cyan-700">
+              <li key={i} className="px-6 hover:text-sky-600">
+                <a href={menu.link}>{menu.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div
+          className={`md:hidden m-1 mx-5  absolute top-0 right-0 text-white ${
+            isOpen ? "" : "text-gray-100"
+          } `}
+          onClick={handleOpenMenu}
+        >
+          {isOpen ? (
+            <AiOutlineClose
+              size={30}
+              className="absolute right-0 z-[999] text-gray-900 "
+            />
+          ) : (
+            <AiOutlineMenuUnfold
+              size={30}
+              className="absolute right-0 z-[999] "
+            />
+          )}
+        </div>
+        <div
+          className={`md:hidden text-gray-900 absolute w-1/2  h-screen px-7 py-2 font-medium bg-white top-0 right-0 ${
+            isOpen ? "right-0" : "right-[-100%]"
+          }`}
+        >
+          <ul className="flex flex-col h-full gap-8 py-2 text-lg">
+            {menuLinks.map((menu, i) => (
+              <li key={i} className="px-6 hover:text-sky-600">
                 <a href={menu.link}>{menu.name}</a>
               </li>
             ))}
